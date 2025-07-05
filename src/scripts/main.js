cdf5b8ed04dcf60b6d2b651657f07c57fb148357
Volodimir <volodimirbryatko@gmail.com>
2025-07-06 00:12:03 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+document.querySelectorAll('#thumbs a').forEach((element) => {
+  element.addEventListener('click', (e) => {
+    document.querySelector('#largeImg').src = element.href;
+    e.preventDefault();
+  });
+});
