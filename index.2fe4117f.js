35619bfc3f185a546d2fb411782579a90c1edea2
Volodimir <volodimirbryatko@gmail.com>
2025-07-06 00:13:00 +0300

    make build

---
+++ index.2fe4117f.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("#thumbs a").forEach(function(e){e.addEventListener("click",function(t){document.querySelector("#largeImg").src=e.href,t.preventDefault()})});
+//# sourceMappingURL=index.2fe4117f.js.map
